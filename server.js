25bd1a6910c1cf1994ff6eb1079eb3ad9dede470
Joshua Dinh <joshuahungdinh@gmail.com>
2021-04-02 12:12:11 -0700

    set up api endpoint routes

--- server.js
+++ server.js
@@ -5,6 +5,12 @@ const app = express();
 // Connect Database
 connectDB();
 
+// Define Routes
+app.use("/api/users", require("./routes/api/users"));
+app.use("/api/post", require("./routes/api/post"));
+app.use("/api/profile", require("./routes/api/profile"));
+app.use("/api/auth", require("./routes/api/auth"));
+
 app.get("/", (req, res) => res.send("api running"));
 
 const port = process.env.Port || 5000;
