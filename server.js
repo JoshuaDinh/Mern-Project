305c2d94b82a9dd89b5909ae00866a150793c98e
Joshua Dinh <joshuahungdinh@gmail.com>
2021-04-03 14:43:51 -0700

    added user schema

--- server.js
+++ server.js
@@ -5,13 +5,13 @@ const app = express();
 // Connect Database
 connectDB();
 
+app.get("/", (req, res) => res.send("api running"));
+
 // Define Routes
 app.use("/api/users", require("./routes/api/users"));
 app.use("/api/post", require("./routes/api/post"));
 app.use("/api/profile", require("./routes/api/profile"));
 app.use("/api/auth", require("./routes/api/auth"));
-
-app.get("/", (req, res) => res.send("api running"));
 
 const port = process.env.Port || 5000;
 
