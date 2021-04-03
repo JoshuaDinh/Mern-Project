f286ed1dcc2720d9b6423fd4c8fbe68942a6079c
Joshua Dinh <joshuahungdinh@gmail.com>
2021-04-03 15:04:01 -0700

    added express validator

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,11 +1,29 @@
 const express = require("express");
 const router = express.Router();
+const { check, validationResult } = require("express-validator");
 
-// @route  GET api/users
-// @desc   Test route
+// @route  Post api/users
+// @desc   Register User
 // @access Public
 // @route  GET api/users
 
-router.get("/", (req, res) => res.send("User Route"));
+router.post(
+  "/",
+  [
+    check("name", "Name is required").not().isEmpty(),
+    check("email", "Please include a valid Email").isEmail(),
+    check(
+      "password",
+      "Please enter a password with 6 or more characters"
+    ).isLength({ min: 6 }),
+  ],
+  (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+    res.send("User Route");
+  }
+);
 
 module.exports = router;
