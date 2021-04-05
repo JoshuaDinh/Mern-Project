a196da49a40ee6029819858119f90a0629728a13
Joshua Dinh <joshuahungdinh@gmail.com>
2021-04-05 11:19:08 -0700

    added jsonwebtoken

--- routes/api/users.js
+++ routes/api/users.js
@@ -3,6 +3,8 @@ const router = express.Router();
 const { check, validationResult } = require("express-validator");
 const gravatar = require("gravatar");
 const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+const config = require("config");
 const User = require("../../models/User");
 
 // @route  Post api/users
@@ -55,7 +57,20 @@ router.post(
       await user.save();
 
       // Return JWT
-      res.send("User registered");
+      const payload = {
+        user: {
+          id: user.id,
+        },
+      };
+      jwt.sign(
+        payload,
+        config.get("jwtSecret"),
+        { expiresIn: 3600000 },
+        (err, token) => {
+          if (err) throw err;
+          res.json({ token });
+        }
+      );
     } catch (err) {
       console.error(err.message);
       res.status(500).send("Server Error");
